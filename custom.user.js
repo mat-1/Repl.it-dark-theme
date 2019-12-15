b90908e9198239b38f1c9a9d11214a368be8d1b6
mat <replit@matdoes.dev>
2019-12-15 16:51:06 +0000

    0.4.9

--- custom.user.js
+++ custom.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name Repl.it Dark Theme
 // @namespace https://repl.it/
-// @version 2.4.7
+// @version 2.4.9
 // @description Custom Repl.it dark themes
 // @author mat
 // @match http*://*.repl.it/*
