52f36c6f0b50ee79ca17176e5944d11f531125a9
mat <replit@matdoes.dev>
2020-08-06 04:48:35 +0000

    i forgot to commit this

--- custom.user.js
+++ custom.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name Repl.it Dark Theme
 // @namespace https://repl.it/
-// @version 2.4.15
+// @version 2.4.19
 // @description Custom Repl.it dark themes
 // @author mat
 // @match http*://*.repl.it/*
@@ -17,6 +17,7 @@
 // ==/UserScript==
 
 var loadDarktheme = (function() {
+	if (document.getElementById('dark-theme')) return
 	var head, style, css, darkThemeScripts;
 	head = document.getElementsByTagName('head')[0];
 	style = document.createElement('style');
@@ -27,9 +28,29 @@ var loadDarktheme = (function() {
 	style.id = 'dark-theme';
 	css = `<%- css %>`;
 	style.innerHTML = css;
-	head.appendChild(style);
-	console.log('Loaded @mat1\'s dark theme 👌')
+	if (head) {
+		head.appendChild(style);
+		console.log('Loaded @mat1\'s dark theme 👌')
+
+		const observer = new MutationObserver((e) => {
+			for (const record of e) {
+				if (record.removedNodes.length) {
+					for (const node of record.removedNodes) {
+						if (node.id == 'dark-theme')
+							loadDarktheme()
+					}
+				}
+			}
+		});
+		observer.observe(style.parentElement, {
+			childList: true
+		})
+
+	} else {
+			console.log('head element not found')
+			document.addEventListener('DOMContentLoaded', loadDarktheme)
+	}
+
 });
 
 loadDarktheme()
-window.addEventListener('load', loadDarktheme, false); // if loading failed, wait until the page fully loads and try again
